9600e2d1bfeee58be89401c8f77258a22a981384
Ennier Manuel Navea Castillo <enavea90@gmail.com>
2023-12-14 20:17:35 +0100

    delete note functionality, and clear form

--- keeper/src/components/CreateArea.jsx
+++ keeper/src/components/CreateArea.jsx
@@ -20,6 +20,11 @@ function CreateArea(props) {
 
   function submitNote(event) {
       props.onAdd(note);
+      setNote(
+        {title: "",
+        content: ""}
+      )
+
       event.preventDefault();
   }
 
